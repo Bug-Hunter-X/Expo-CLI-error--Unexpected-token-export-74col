63257eab3812a4baf4adacbdb52ad8b0219a3af5
BugHunter-X <askzenai@gmail.com>
2024-12-25 13:46:04 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default App; // error is here, it should be export default function App(){}
